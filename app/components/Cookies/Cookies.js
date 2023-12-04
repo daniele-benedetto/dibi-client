2bf8422d4966dcb2d2cd7852a69621012812a31b
Daniele Benedetto <daniele.benedetto@appius.it>
2023-12-04 20:36:23 +0100

    fix cookies

--- app/components/Cookies/Cookies.js
+++ app/components/Cookies/Cookies.js
@@ -3,8 +3,11 @@ import { useLocalStorage } from "@/app/hooks/useLocalStorage";
 import { useState } from "react";
 import Button from '../Button/Button';
 import { AiOutlineClose } from "react-icons/ai";
+import {useRouter} from 'next/navigation';
 
 export default function Cookies() {
+
+    const router = useRouter();
 
     const [cookiesOpen, setCookiesOpen] = useState(true);
     const [storageCookies, setStorageCookies] = useLocalStorage("cookies", false);
@@ -20,9 +23,10 @@ export default function Cookies() {
                 <p className="text-sm font-bold max-w-5xl">
                     La tua privacy è importante per noi! Utilizziamo solo cookie tecnici indispensabili per garantire il corretto funzionamento del nostro sito e migliorare la tua esperienza di navigazione.<br/>
                     I cookie tecnici sono essenziali per attività quali l'autenticazione, la gestione della sessione e la memorizzazione delle preferenze dell'utente. Questi cookie non raccolgono informazioni personali e non possono essere disabilitati attraverso questa finestra di dialogo.<br/>
-                    Se desideri ulteriori informazioni o gestire le tue preferenze sui cookie, clicca su "Ulteriori informazioni" per accedere alla nostra informativa sulla privacy.<br/>
+                    Se desideri ulteriori informazioni, clicca su "Ulteriori informazioni" per accedere alla nostra informativa sulla privacy.<br/>
                 </p>
                 <div className="w-full max-w-5xl flex justify-end">
+                    <Button text={'ulteriori informazioni'} type={'underline'} action={() => router.push('/privacy-policy')} />
                     <Button text={'Chiudi'} type={'container'} action={handleClick} />
                 </div>
             </section> )}

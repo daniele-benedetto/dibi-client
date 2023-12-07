37a218b10b7bc0290174977d80048b55514b69aa
Daniele Benedetto <daniele.benedetto@appius.it>
2023-12-07 02:24:53 +0100

    fix

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -127,8 +127,8 @@ export default function Navbar({navbar, categories}) {
                         { totalQty > 0 && <motion.span animate={{scale: 1}} initial={{scale: 0}} className="absolute -top-2 -left-2 -translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full w-4 h-4 flex items-center justify-center text-white text-xs">{totalQty}</motion.span> }
                     </div>
                     <div className="relative">
-                        {!user && <FaUserAlt color="#F68129" onClick={() => setUserMenuIsOpen(!userMenuIsOpen)} size={24} className="cursor-pointer" />}
-                        {user && <span onClick={() => setUserMenuIsOpen(!userMenuIsOpen)} className="flex items-center justify-center rounded-full text-gray-700 text-md cursor-pointer">{email}</span>}
+                        {!user && <FaUserAlt color="#F68129" onClick={() => setUserMenuIsOpen(!userMenuIsOpen)} size={24} className="cursor-pointer mx-2" />}
+                        {user && <span onClick={() => setUserMenuIsOpen(!userMenuIsOpen)} className="flex items-center justify-center rounded-full text-gray-700 text-md cursor-pointer mx-2 underline">{email}</span>}
                         { userMenuIsOpen && <div className="absolute top-full translate-y-3 right-3 bg-white shadow-md rounded-md p-5 w-72 max-w-md z-10">
                             <AiOutlineClose onClick={() => setUserMenuIsOpen(false)} size={16} className="cursor-pointer absolute top-1 right-1" />
                             { !user  && <LoginForm setUserMenuIsOpen={setUserMenuIsOpen} /> }

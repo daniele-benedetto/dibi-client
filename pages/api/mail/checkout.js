a34f1254e242829c4c2f6f774eb8aa858c73352e
Daniele Benedetto <daniele.benedetto@appius.it>
2024-04-23 09:29:44 +0200

    fix mail

--- pages/api/mail/checkout.js
+++ pages/api/mail/checkout.js
@@ -1,50 +1,50 @@
-const mail = require('@sendgrid/mail');
+const mail = require("@sendgrid/mail");
 
-mail.setApiKey('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');
+mail.setApiKey("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");
 
 export default async (req, res) => {
-    try {
-        const body = JSON.parse(req.body);
-        const name = body.name;
-        const address = body.address;
-        const products = body.products;
-        const email = body.email;
-        const total = body.total;
+	try {
+		const body = JSON.parse(req.body);
+		const name = body.name;
+		const address = body.address;
+		const products = body.products;
+		const email = body.email;
+		const total = body.total;
 
-        let prodotti = '';
-        products.map((p) => {
-            prodotti += `${p.name} - ${p.quantity} \n`;
-        });
+		let prodotti = "";
+		products.map(p => {
+			prodotti += `${p.name} - ${p.quantity} \n`;
+		});
 
-        const subject = 'Cianfrusalia - Nuovo messaggio dal modulo contatti';
+		const subject = "Cianfrusalia - Nuovo messaggio dal modulo contatti";
 
-        const message = {
-            to: email,
-            from: 'daniele.benedetto@appius.it',
-            subject: subject,
-            templateId: 'd-ff1ced243ae14886924493f4b7a8f920',
-            dynamicTemplateData: {
-                first_name: name,
-                address_line_1: address,
-                line: prodotti,
-            }
-        };
+		const message = {
+			to: email,
+			from: "mrcianfrusalia@cianfrusalia.it",
+			subject: subject,
+			templateId: "d-0a76064bc06943b8b3e296a4f21fb9ba",
+			dynamicTemplateData: {
+				first_name: name,
+				address_line_1: address,
+				line: prodotti,
+			},
+		};
 
-        await mail.send(message);
+		await mail.send(message);
 
-        const message2 = {
-            to: 'daniele.benedetto@appius.it',
-            from: 'daniele.benedetto@appius.it',
-            subject: 'Cianfrusalia - Nuovo ordine',
-            text: `È stato effettuato un nuovo ordine da ${name} per un totale di ${total.toFixed(2)}€.
+		const message2 = {
+			to: "mrcianfrusalia@cianfrusalia.it",
+			from: "mrcianfrusalia@cianfrusalia.it",
+			subject: "Cianfrusalia - Nuovo ordine",
+			text: `È stato effettuato un nuovo ordine da ${name} per un totale di ${total.toFixed(2)}€.
             Per visualizzare i dettagli dell'ordine visita il pannello di amministrazione.`,
-        };
+		};
 
-        await mail.send(message2);
+		await mail.send(message2);
 
-        res.status(200).json({ status: 'Ok' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'Error', error: error.message });
-    }
+		res.status(200).json({ status: "Ok" });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ status: "Error", error: error.message });
+	}
 };
--- pages/api/mail/contact.js
+++ pages/api/mail/contact.js
@@ -1,22 +1,22 @@
-const mail = require('@sendgrid/mail');
+const mail = require("@sendgrid/mail");
 
-mail.setApiKey('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');
+mail.setApiKey("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");
 
 export default async (req, res) => {
-    const body = JSON.parse(req.body);
+	const body = JSON.parse(req.body);
 
-    const id = body.id;
+	const id = body.id;
 
-    const subject = 'Cianfrusalia - Nuovo messaggio dal modulo contatti';
-    const message = `Ciao Diego, è stato inviato un nuovo messaggio dal modulo contatti del sito Cianfrusaglia.it. Puoi visualizzarlo qui: https://www.cianfrusalia.it/backend/admin/content-manager/collectionType/api::contact.contact/${id}`;
+	const subject = "Cianfrusalia - Nuovo messaggio dal modulo contatti";
+	const message = `Ciao Diego, è stato inviato un nuovo messaggio dal modulo contatti del sito Cianfrusaglia.it. Puoi visualizzarlo qui: https://www.cianfrusalia.it/backend/admin/content-manager/collectionType/api::contact.contact/${id}`;
 
-    await mail.send({
-        to: 'daniele.benedetto@appius.it',
-        from: 'daniele.benedetto@appius.it',
-        subject: subject,
-        text: message,
-        html: message.replace(/\r\n/g, '<br>'),
-    });
+	await mail.send({
+		to: "mrcianfrusalia@cianfrusalia.it",
+		from: "mrcianfrusalia@cianfrusalia.it",
+		subject: subject,
+		text: message,
+		html: message.replace(/\r\n/g, "<br>"),
+	});
 
-    res.status(200).json({ status: 'Ok' });
-}
+	res.status(200).json({ status: "Ok" });
+};
